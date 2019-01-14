48a7abc9855bccb298179f319a9fab0ff5d6c48a
Ben <li.benjamin.b@gmail.com>
2019-01-14 18:20:49 -0500

    added arrows, currently not functional, fixed visual bugs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 (function($){
-$(function(){
+    $(function(){
 
-    $('#projects-large').carousel({
-        fullWidth: true,
-        indicators: true
-    });
-    $('#projects-small').carousel();
-    $('.parallax').parallax();
-    $('.modal').modal();
+        $('#projects-large').carousel({
+            fullWidth: true,
+            indicators: true
+        });
+        $('.next').click(function() {
+            $('.carousel.carousel-slider').carousel('next');
+        });
+        $('.prev').click(function() {
+            $('.carousel.carousel-slider').carousel('prev');
+        });
 
-}); // end of document ready
+        $('.parallax').parallax();
+        $('.modal').modal();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
